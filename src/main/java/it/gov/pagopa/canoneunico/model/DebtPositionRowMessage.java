7da55716734ac1be96d3ca83f7bf6a2ff27c051f
Jacopo <jacopo.carlini@gmail.com>
2022-03-03 14:36:37 +0100

    added model and entit

---
+++ src/main/java/it/gov/pagopa/canoneunico/model/DebtPositionRowMessage.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.canoneunico.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DebtPositionRowMessage {
+
+    // CSV
+    private String debtorName;
+    private String debtorEmail;
+    private Long amount;
+
+    // generated
+    private String iuv;
+    private String iupd;
+
+    // EC config
+    private String fiscalCode;
+    private String companyName;
+    private String iban;
+
+}
