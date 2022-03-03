78a68815361e790a86ae049f6238ce917d08b22b
Jacopo <jacopo.carlini@gmail.com>
2022-03-03 14:36:37 +0100

    added model and entit

---
+++ src/main/java/it/gov/pagopa/canoneunico/entity/DebtPositionEntity.java
@@ -0,0 +1,50 @@
+package it.gov.pagopa.canoneunico.entity;
+
+import com.microsoft.azure.storage.table.TableServiceEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DebtPositionEntity extends TableServiceEntity {
+
+    private Status status;
+
+    // CSV
+    private String paIdIstat;
+    private String paIdCatasto;
+    private String paIdFiscalCode;
+    private String paIdCbill;
+    private String paPecEmail;
+    private String paReferentEmail;
+    private String paReferentName;
+    private String debtorIdFiscalCode;
+    private String paymentNoticeNumber;
+    private String note;
+
+    private String debtorName;
+    private String debtorEmail;
+    private Long amount;
+
+    // generated
+    private String iuv;
+    private String iupd;
+
+    // EC config
+    private String fiscalCode;
+    private String companyName;
+    private String iban;
+
+
+    public DebtPositionEntity(String filename, String id) {
+        this.partitionKey = filename;
+        this.rowKey = id;
+    }
+
+}
