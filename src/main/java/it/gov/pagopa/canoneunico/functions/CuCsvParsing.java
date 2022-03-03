3cb821c88a69de72ac4ae50ae9a4131b44bf9281
Jacopo <jacopo.carlini@gmail.com>
2022-03-03 21:51:47 +0100

    [PPD-95] implemented logic

--- src/main/java/it/gov/pagopa/canoneunico/functions/CuCsvParsing.java
+++ src/main/java/it/gov/pagopa/canoneunico/functions/CuCsvParsing.java
@@ -1,25 +1,24 @@
 package it.gov.pagopa.canoneunico.functions;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.BlobTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Azure Functions with Azure Blob trigger.
  */
 public class CuCsvParsing {
-    private String storageConnectionString = System.getenv("CU_SA_CONNECTION_STRING");
+    private final String storageConnectionString = System.getenv("CU_SA_CONNECTION_STRING");
 
     /**
      * This function will be invoked when a new or updated blob is detected at the
      * specified path. The blob contents are provided as input to this function.
-     *
      */
     @FunctionName("CuCsvParsingFunction")
     public void run(
