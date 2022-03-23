1da85019887f01a74893eec1547ab9194fca7491
Jacopo <jacopo.carlini@gmail.com>
2022-03-23 09:39:58 +0100

    added test

--- src/test/java/it/gov/pagopa/canoneunico/functions/CuCreateDebtPositionTest.java
+++ src/test/java/it/gov/pagopa/canoneunico/functions/CuCreateDebtPositionTest.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.canoneunico.functions;
 import com.microsoft.azure.functions.ExecutionContext;
 import it.gov.pagopa.canoneunico.model.DebtPositionMessage;
 import it.gov.pagopa.canoneunico.model.DebtPositionRowMessage;
+import it.gov.pagopa.canoneunico.service.DebtPositionQueueService;
 import it.gov.pagopa.canoneunico.service.DebtPositionTableService;
 import it.gov.pagopa.canoneunico.service.GpdClient;
 import org.junit.jupiter.api.Test;
@@ -40,6 +41,9 @@ class CuCreateDebtPositionTest {
     @Mock
     DebtPositionTableService tableService;
 
+    @Mock
+    DebtPositionQueueService queueService;
+
 
     @Test
     void run() throws JsonProcessingException {
@@ -55,6 +59,46 @@ class CuCreateDebtPositionTest {
 
         String message = new ObjectMapper().writeValueAsString(DebtPositionMessage.builder()
                 .csvFilename("csv")
+                .rows(List.of(DebtPositionRowMessage.builder()
+                        .id("001")
+                        .amount(100L)
+                        .iuv("IUV")
+                        .iupd("IUPD")
+                        .paIdFiscalCode("PAFISCALCODE")
+                        .debtorIdFiscalCode("DEBTORFISCALCODE")
+                        .debtorName("DEBTORNAME")
+                        .debtorEmail("DEBTOREMAIL")
+                        .companyName("COMPANY")
+                        .iban("IBAN")
+                        .retryAction("NONE")
+                        .build()))
+                .build());
+        function.run(message, context);
+
+        // Asserts
+        verify(function, times(2)).getGpdClientInstance();
+        verify(function, times(1)).getDebtPositionTableService(logger);
+        verify(gpdClient, times(1)).createDebtPosition(any(), any(), any());
+        verify(gpdClient, times(1)).publishDebtPosition(any(), any(), any());
+        verify(tableService, times(1)).updateEntity(anyString(), any(), anyBoolean());
+
+    }
+
+    @Test
+    void runFailed() throws JsonProcessingException {
+        // general var
+        Logger logger = Logger.getLogger("testlogging");
+
+        // precondition
+        when(context.getLogger()).thenReturn(logger);
+        doReturn(gpdClient).when(function).getGpdClientInstance();
+        when(gpdClient.createDebtPosition(any(), any(), any())).thenReturn(true);
+        when(gpdClient.publishDebtPosition(any(), any(), any())).thenReturn(false);
+        doReturn(tableService).when(function).getDebtPositionTableService(logger);
+
+        String message = new ObjectMapper().writeValueAsString(DebtPositionMessage.builder()
+                .csvFilename("csv")
+                        .retryCount(0)
                 .rows(List.of(DebtPositionRowMessage.builder()
                         .id("001")
                         .amount(100L)
