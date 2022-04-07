d04c48d8ca2dad1c9c808d83e73a135dea11ab99
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2022-04-07 17:21:18 +0200

    [fix check]: added check id_istat and id_catasto

--- src/main/java/it/gov/pagopa/canoneunico/csv/validaton/PaymentNoticeVerifier.java
+++ src/main/java/it/gov/pagopa/canoneunico/csv/validaton/PaymentNoticeVerifier.java
@@ -44,7 +44,32 @@ public class PaymentNoticeVerifier implements BeanVerifier<PaymentNotice>{
 		
 		// check existence of the organization fiscal code if valued in csv file
 		if (null != bean.getPaIdFiscalCode() && !bean.getPaIdFiscalCode().isBlank() && !checkIsPresentOrganizationFiscalCode(bean.getPaIdFiscalCode())) {
-			errors.add("Not found the pa_id_fiscal_code ["+bean.getPaIdFiscalCode()+"].");
+			errors.add("Not found the pa_id_fiscal_code ["+bean.getPaIdFiscalCode()+"] in ec config table.");
+		}
+		
+		// check existence of id istat if valued in csv file
+		if (null != bean.getPaIdIstat() && !bean.getPaIdIstat().isBlank() && !checkIsPresentIdIstat(bean.getPaIdIstat())) {
+			errors.add("Not found the pa_id_istat ["+bean.getPaIdIstat()+"] in ec config table.");
+		}
+		
+		// check existence of id catasto if valued in csv file
+		if (null != bean.getPaIdCatasto() && !bean.getPaIdCatasto().isBlank() && !checkIsPresentIdCatasto(bean.getPaIdCatasto())) {
+			errors.add("Not found the pa_id_catasto ["+bean.getPaIdCatasto()+"] in ec config table.");
+		}
+		
+		// check duplication organization fiscal code if valued in csv file
+		if (null != bean.getPaIdFiscalCode() && !bean.getPaIdFiscalCode().isBlank() && !checkDuplicatedOrganizationFiscalCode(bean.getPaIdFiscalCode())) {
+			errors.add("Duplicated pa_id_fiscal_code ["+bean.getPaIdFiscalCode()+"] found in ec config table.");
+		}
+		
+		// check duplication id istat if valued in csv file
+		if (null != bean.getPaIdIstat() && !bean.getPaIdIstat().isBlank() && !checkDuplicatedIdIstat(bean.getPaIdIstat())) {
+			errors.add("Duplicated pa_id_istat ["+bean.getPaIdIstat()+"] found in ec config table.");
+		}
+		
+		// check duplication id catasto code if valued in csv file
+		if (null != bean.getPaIdCatasto() && !bean.getPaIdCatasto().isBlank() && !checkDuplicatedIdCatasto(bean.getPaIdCatasto())) {
+			errors.add("Duplicated pa_id_catasto ["+bean.getPaIdCatasto()+"] found in ec config table.");
 		}
 		
 		if (!errors.isEmpty()) {
@@ -57,6 +82,31 @@ public class PaymentNoticeVerifier implements BeanVerifier<PaymentNotice>{
 	private boolean checkIsPresentOrganizationFiscalCode(String paIdFiscalCode) {
 		Optional<EcConfigEntity> ecConfig = organizationsList.stream().filter(o -> o.getRowKey().equals(paIdFiscalCode)).findFirst();
 		return ecConfig.isPresent();
+	}
+	
+	private boolean checkIsPresentIdIstat(String idIstat) {
+		Optional<EcConfigEntity> ecConfig = organizationsList.stream().filter(o -> o.getPaIdIstat().equals(idIstat)).findFirst();
+		return ecConfig.isPresent();
+	}
+	
+	private boolean checkIsPresentIdCatasto(String idCatasto) {
+		Optional<EcConfigEntity> ecConfig = organizationsList.stream().filter(o -> o.getPaIdCatasto().equals(idCatasto)).findFirst();
+		return ecConfig.isPresent();
+	}
+	
+	private boolean checkDuplicatedOrganizationFiscalCode(String paIdFiscalCode) {
+		long numOccurence = organizationsList.stream().filter(o -> o.getRowKey().equals(paIdFiscalCode)).count();
+		return numOccurence < 2;
+	}
+	
+	private boolean checkDuplicatedIdIstat(String idIstat) {
+		long numOccurence = organizationsList.stream().filter(o -> o.getPaIdIstat().equals(idIstat)).count();
+		return numOccurence < 2;
+	}
+	
+	private boolean checkDuplicatedIdCatasto(String idCatasto) {
+		long numOccurence = organizationsList.stream().filter(o -> o.getPaIdCatasto().equals(idCatasto)).count();
+		return numOccurence < 2;
 	}
 
 }
